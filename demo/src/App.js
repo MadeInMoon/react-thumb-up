0c50b48342fa98cac2f0deb7a1121b8348d80726
MaxInMoon <max@madeinmoon.io>
2018-11-25 01:39:49 +0100

    update demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -6,11 +6,24 @@ import './App.scss';
 
 type PropsType = {||};
 
+type StateType = {|
+  controlledThumbIsActive: boolean,
+|};
 
-class App extends Component<PropsType> {
+
+class App extends Component<PropsType, StateType> {
+
+  state = {
+    controlledThumbIsActive: false,
+  }
 
   onClick = () => {
     console.log('onClick');
+  }
+
+  onClickControlled = (isActive: boolean) => {
+    this.setState({ controlledThumbIsActive: isActive });
+
   }
 
   render() {
@@ -37,17 +50,60 @@ class App extends Component<PropsType> {
             Star
           </a>
         </div>
-        <ThumbUp
-          className="thumbUp"
-          animateOnMount
-          // onClick={this.onClick}
-          shirtColor="white"
-          handColor="#ef4040"
-          // thumbColor={this.state.thumbColor}
-          // shirtColor={this.state.shirtColor}
-          // handColor={this.state.handColor}
-          // thumbFillOpacity={this.state.thumbFillOpacity}
-        />
+        <div className="middle">
+            <div className="thumb-wrapper">
+              <p className="thumb-label">Uncontrolled</p>
+              <ThumbUp
+                className="thumbUp"
+                animateOnMount
+                // onClick={this.onClick}
+                thumbStyles={{
+                  shirtColor: 'white',
+                  handColor: '#ef4040',
+                  // fillOpacity: 1,
+                }}
+                // thumbColor={this.state.thumbColor}
+                // shirtColor={this.state.shirtColor}
+                // handColor={this.state.handColor}
+                // thumbFillOpacity={this.state.thumbFillOpacity}
+              />
+            </div>
+            <div className="thumb-wrapper">
+              <p className="thumb-label">Controlled</p>
+              <ThumbUp
+                className="thumbUp"
+                controlled
+                active={this.state.controlledThumbIsActive}
+                onClick={this.onClickControlled}
+                // thumbStyles={{
+                //   active: {
+                //     shirtColor: 'white',
+                //     handColor: '#ef4040',
+                //     fillOpacity: 1,
+                //   },
+                //   unactive: {
+                //     shirtColor: 'white',
+                //     handColor: 'white',
+                //     fillOpacity: 0.3,
+                //   },
+                // }}
+              />
+            </div>
+          <div className="thumb-wrapper">
+            <p className="thumb-label">Size {'{50}'}</p>
+            <ThumbUp
+              className="thumbUp"
+              size={100}
+              style={{
+                height: '200px',
+              }}
+              // circleStyle={{
+              //   width: '50px',
+              //   height: '50px',
+              // }}
+            />
+          </div>
+        </div>
         <div className="bottom">
           <h3>
             Wrapped with <span role="img" aria-label="love">❤️</span> by
